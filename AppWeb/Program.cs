84894413ec2e6e8e4e10ff351586ca1489770c82
Pallabi_Biswas <pallabi_biswas@dell.com>
2022-08-16 16:31:08 +0530

    Update test

--- AppWeb/Program.cs
+++ AppWeb/Program.cs
@@ -1,13 +1,28 @@
+using AppWeb.Models;
+using AppWeb.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.Configure<StudentStoreDatabaseSettings>(
+//                builder.Configuration.GetSection(nameof(StudentStoreDatabaseSettings)));
+
+//builder.Services.AddSingleton<IStudentStoreDatabaseSettings>(sp =>
+//    sp.GetRequiredService<IOptions<StudentStoreDatabaseSettings>>().Value);
+
+//builder.Services.AddSingleton<IMongoClient>(s =>
+//        new MongoClient(builder.Configuration.GetValue<string>("StudentStoreDatabaseSettings:ConnectionString")));
+
+//builder.Services.AddScoped<IStudentService, StudentService>();
 
 namespace AppWeb
 {
